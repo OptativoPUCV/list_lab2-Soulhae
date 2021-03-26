21e571f58c5559a1e169dc65ccbfd2dd66e1e3b2
Replit user <replituser@example.com>
2021-03-26 19:32:13 +0000

    new try

--- list.c
+++ list.c
@@ -75,7 +75,10 @@ void pushBack(List * list, const void * data) {
 void pushCurrent(List * list, const void * data) {
   Node *nodo = createNode(data);
   if(!list->head) list->head = nodo;
-  if(list->current == list->head) list->head->next = nodo;
+  if(list->current == list->head){
+    list->head->next = nodo;
+    nodo->prev = list->head;
+  }
   list->current->next = nodo;
 }
 
